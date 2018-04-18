09a40c663dbfb5955b370d40be7c608798e04bc1
akosgo <33903695+akosgo@users.noreply.github.com>
2018-04-18 18:25:51 +0200

    Test Kml file

--- data processing/src/readKML.py
+++ data processing/src/readKML.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-tree = ET.parse("../data/velib.kml")
+tree = ET.parse("../../data/velib.kml")
 root = tree.getroot();
+print("coucou")
+
+#for child in root.findall
